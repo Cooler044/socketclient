462d7460ef345ebc2fcd3fac70d3464565794667
Dmitry Sidorenko <dm.sidorenko@surc.local>
2016-11-10 18:53:16 +0200

    handler thread added

--- app/src/main/java/example/socketclient/ConnectionActivity.java
+++ app/src/main/java/example/socketclient/ConnectionActivity.java
@@ -1,10 +1,29 @@
 package example.socketclient;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class ConnectionActivity extends Activity {
+
+    //Strings to register to create intent filter for registering the recivers
+    private static final String ACTION_STRING_SERVICE = "ToService";
+    private static final String ACTION_STRING_ACTIVITY = "ToActivity";
+    public final static String EXTRA_ADDRESS = "example.socketclient.ADDRESS";
+    public final static String EXTRA_PORT = "example.socketclient.PORT";
+
+    private BroadcastReceiver activityReceiver = new BroadcastReceiver() {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.d("Activity", "received message in activity!");
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +34,40 @@ public class ConnectionActivity extends Activity {
         String address = intent.getStringExtra(LoginActivity.EXTRA_ADDRESS);
         String port = intent.getStringExtra(LoginActivity.EXTRA_PORT);
 
+        Log.d("ConnectionActivity", address + ":" + port);
+
+        //STEP2: register the receiver
+        if (activityReceiver != null) {
+            //Create an intent filter to listen to the broadcast sent with the action "ACTION_STRING_ACTIVITY"
+            IntentFilter intentFilter = new IntentFilter(ACTION_STRING_ACTIVITY);
+            //Map the intent filter to the receiver
+            registerReceiver(activityReceiver, intentFilter);
+        }
+
+        //Start the service on launching the application
+        Intent anotherIntent = new Intent(this, ConnectionService.class);
+        anotherIntent.putExtra(EXTRA_ADDRESS, address);
+        anotherIntent.putExtra(EXTRA_PORT, port);
+        startService(anotherIntent);
+
+    }
+
+    //send broadcast from activity to all receivers listening to the action "ACTION_STRING_SERVICE"
+    private void sendBroadcast() {
+        Intent new_intent = new Intent();
+        new_intent.setAction(ACTION_STRING_SERVICE);
+        sendBroadcast(new_intent);
+    }
+
+    public void SendMessage(View v) {
+        sendBroadcast();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d("Service", "onDestroy");
+        //STEP3: Unregister the receiver
+        unregisterReceiver(activityReceiver);
     }
 }
--- app/src/main/java/example/socketclient/ConnectionService.java
+++ app/src/main/java/example/socketclient/ConnectionService.java
@@ -1,11 +1,18 @@
 package example.socketclient;
 
 import android.app.Service;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.AsyncTask;
 import android.os.Binder;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.os.IBinder;
+import android.os.Looper;
+import android.os.Message;
 import android.util.Log;
 
 import java.io.BufferedReader;
@@ -19,181 +26,286 @@ import java.net.Socket;
 
 public class ConnectionService extends Service {
 
-    String acc_email;
-    private final IBinder mBinder = new LocalBinder();
+    //Strings to register to create intent filter for registering the recivers
+    private static final String ACTION_STRING_SERVICE = "ToService";
+    private static final String ACTION_STRING_ACTIVITY = "ToActivity";
 
-    public class LocalBinder extends Binder {
-        ConnectionService getService() {
-            return ConnectionService.this;
+    //STEP1: Create a broadcast receiver
+    private BroadcastReceiver serviceReceiver = new BroadcastReceiver() {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.d("Service", "Receive broadcast from activity");
         }
+    };
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        String address = intent.getStringExtra(ConnectionActivity.EXTRA_ADDRESS);
+        String port = intent.getStringExtra(ConnectionActivity.EXTRA_PORT);
+
+        Log.d("Service", address + ":" + port);
+
+        openConnection(address, port);
+
+        return START_STICKY;
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
 
-        startService();
+        //STEP2: register the receiver
+        if (serviceReceiver != null) {
+            //Create an intent filter to listen to the broadcast sent with the action "ACTION_STRING_SERVICE"
+            IntentFilter intentFilter = new IntentFilter(ACTION_STRING_SERVICE);
+            //Map the intent filter to the receiver
+            registerReceiver(serviceReceiver, intentFilter);
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d("Service", "onDestroy");
+        //STEP3: Unregister the receiver
+        unregisterReceiver(serviceReceiver);
+    }
+
+    //send broadcast from activity to all receivers listening to the action "ACTION_STRING_ACTIVITY"
+    private void sendBroadcast() {
+        Intent new_intent = new Intent();
+        new_intent.setAction(ACTION_STRING_ACTIVITY);
+        sendBroadcast(new_intent);
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        return mBinder;
+        return null;
     }
 
-    @Override
-    public void onDestroy() { }
+    class connectionMaintainerThread extends HandlerThread {
 
-    // Init variables and open socket
-    private void startService() {
+        private Handler mHandler;
+        String mAddress;
+        String mPort;
 
-        acc_email = "test@test.com";
-
-        try {
-            openConnection();
-
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        connectionMaintainerThread(String address, String port) {
+            super("connectionMaintainerThread");
+            mAddress = address;
+            mPort = port;
         }
-    }
 
-    // this method opens connection
-    public void openConnection() throws InterruptedException
-    {
-        try {
-            WatchData data = new WatchData();
-            data.email = acc_email;
-            data.ctx = this;
+        @Override
+        protected void onLooperPrepared() {
+            super.onLooperPrepared();
 
-            //new thread for socket connection
-            new WatchSocket().execute(data);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    //our data class
-    class WatchData
-    {
-        String email;
-        Context ctx;
-    }
-
-    class SocketData
-    {
-        Socket sock;
-        Context ctx;
-    }
-
-    class WatchSocket extends AsyncTask<WatchData , Integer, Integer>
-    {
-        Context mCtx;
-        Socket mySock;
-
-        protected void onProgressUpdate(Integer... progress)
-        { }
-
-        protected void onPostExecute(Integer result)
-        {
-
-        }
-        protected Integer doInBackground(WatchData... param)
-        {
-            InetAddress serverAddr;
-
-            mCtx = param[0].ctx;
-            String email = param[0].email;
-
-            try {
-                while(true)
-                {
-                    serverAddr = InetAddress.getByName("192.168.0.10");
-                    mySock = new Socket(serverAddr, 4505);
-
-                    SocketData data = new SocketData();
-                    data.ctx = mCtx;
-                    data.sock = mySock;
-
-                    GetPacket pack = new GetPacket();
-                    AsyncTask<SocketData, Integer, Integer> running = pack.execute(data);
-
-                    String message = email;
-
-                    try {
-                        PrintWriter out = new PrintWriter( new BufferedWriter( new OutputStreamWriter(mySock.getOutputStream())),true);
-
-                        out.println(message);
-
-                    } catch(Exception e){
-
-                    }
-
-                    while(running.getStatus().equals(AsyncTask.Status.RUNNING))
-                    {
-
-                    }
-
-                    //if asynctasc finished, connection is broken. Close the socket and restart it in while(true)
-                    try
-                    {
-                        mySock.close();
-                    }
-                    catch(Exception e)
-                    {}
+            mHandler = new Handler() {
+                public void handleMessage(Message msg) {
+                    Log.e("MaintainerThread", "handleMessage");
                 }
-            } catch (Exception e) {
-                return -1;
-            }
+            };
         }
     }
 
-    class GetPacket extends AsyncTask<SocketData, Integer, Integer>
-    {
-        Context mCtx;
-        char[] mData;
-        Socket mySock;
+    public void openConnection(String address, String port) {
+        connectionMaintainerThread connectionMaintainer = new connectionMaintainerThread(address, port);
+        connectionMaintainer.start();
 
-        protected void onProgressUpdate(Integer... progress)
-        {
-            try
-            {
-                String prop = String.valueOf(mData);
-                //process message as you like
-            }
-            catch(Exception e)
-            {
-                Log.e("Error", "Socket error: " + e.getMessage());
-            }
-        }
+        Looper loop = connectionMaintainer.getLooper();
+        Handler connectionHandler = new Handler(loop);
 
-        protected void onPostExecute(Integer result)
-        {
-// Это выполнится после завершения работы потока
-        }
-
-        protected Integer doInBackground(SocketData... param)
-        {
-            mySock = param[0].sock;
-            mCtx = param[0].ctx;
-            mData = new char[4096];
-
-            try {
-                BufferedReader reader = new BufferedReader(new InputStreamReader(mySock.getInputStream()));
-                int read = 0;
-
-                while ((read = reader.read(mData)) >= 0 && !isCancelled())
-                {
-                    //call onprogressupdate
-                    if(read > 0) publishProgress(read);
-                }
-                reader.close();
-            } catch (IOException e) {
-                return -1;
-            }
-            catch (Exception e) {
-                return -1;
-            }
-            return 0;
-        }
+        Message msg = new Message();
+        Bundle b = new Bundle();
+        b.putInt("what", 5); // for example
+        msg.setData(b);
+        connectionHandler.sendMessage(msg);
     }
 }
+
+
+
+
+
+
+
+//public class OLOLOService extends Service {
+//
+//    String acc_email;
+//    private final IBinder mBinder = new LocalBinder();
+//
+//    public class LocalBinder extends Binder {
+//        ConnectionService getService() {
+//            return ConnectionService.this;
+//        }
+//    }
+//
+//    @Override
+//    public void onCreate() {
+//        super.onCreate();
+//
+//        startService();
+//    }
+//
+//    @Override
+//    public IBinder onBind(Intent intent) {
+//        return mBinder;
+//    }
+//
+//    @Override
+//    public void onDestroy() { }
+//
+//    // Init variables and open socket
+//    private void startService() {
+//
+//        acc_email = "test@test.com";
+//
+//        try {
+//            openConnection();
+//
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    // this method opens connection
+//    public void openConnection() throws InterruptedException
+//    {
+//        try {
+//            WatchData data = new WatchData();
+//            data.email = acc_email;
+//            data.ctx = this;
+//
+//            //new thread for socket connection
+//            new WatchSocket().execute(data);
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    //our data class
+//    class WatchData
+//    {
+//        String email;
+//        Context ctx;
+//    }
+//
+//    class SocketData
+//    {
+//        Socket sock;
+//        Context ctx;
+//    }
+//
+//    class WatchSocket extends AsyncTask<WatchData , Integer, Integer>
+//    {
+//        Context mCtx;
+//        Socket mySock;
+//
+//        protected void onProgressUpdate(Integer... progress)
+//        { }
+//
+//        protected void onPostExecute(Integer result)
+//        {
+//
+//        }
+//        protected Integer doInBackground(WatchData... param)
+//        {
+//            InetAddress serverAddr;
+//
+//            mCtx = param[0].ctx;
+//            String email = param[0].email;
+//
+//            try {
+//                while(true)
+//                {
+//                    serverAddr = InetAddress.getByName("192.168.0.10");
+//                    mySock = new Socket(serverAddr, 4505);
+//
+//                    SocketData data = new SocketData();
+//                    data.ctx = mCtx;
+//                    data.sock = mySock;
+//
+//                    GetPacket pack = new GetPacket();
+//                    AsyncTask<SocketData, Integer, Integer> running = pack.execute(data);
+//
+//                    String message = email;
+//
+//                    try {
+//                        PrintWriter out = new PrintWriter( new BufferedWriter( new OutputStreamWriter(mySock.getOutputStream())),true);
+//
+//                        out.println(message);
+//
+//                    } catch(Exception e){
+//
+//                    }
+//
+//                    while(running.getStatus().equals(AsyncTask.Status.RUNNING))
+//                    {
+//
+//                    }
+//
+//                    //if asynctasc finished, connection is broken. Close the socket and restart it in while(true)
+//                    try
+//                    {
+//                        mySock.close();
+//                    }
+//                    catch(Exception e)
+//                    {}
+//                }
+//            } catch (Exception e) {
+//                return -1;
+//            }
+//        }
+//    }
+//
+//    class GetPacket extends AsyncTask<SocketData, Integer, Integer>
+//    {
+//        Context mCtx;
+//        char[] mData;
+//        Socket mySock;
+//
+//        protected void onProgressUpdate(Integer... progress)
+//        {
+//            try
+//            {
+//                String prop = String.valueOf(mData);
+//                //process message as you like
+//            }
+//            catch(Exception e)
+//            {
+//                Log.e("Error", "Socket error: " + e.getMessage());
+//            }
+//        }
+//
+//        protected void onPostExecute(Integer result)
+//        {
+//// Это выполнится после завершения работы потока
+//        }
+//
+//        protected Integer doInBackground(SocketData... param)
+//        {
+//            mySock = param[0].sock;
+//            mCtx = param[0].ctx;
+//            mData = new char[4096];
+//
+//            try {
+//                BufferedReader reader = new BufferedReader(new InputStreamReader(mySock.getInputStream()));
+//                int read = 0;
+//
+//                while ((read = reader.read(mData)) >= 0 && !isCancelled())
+//                {
+//                    //call onprogressupdate
+//                    if(read > 0) publishProgress(read);
+//                }
+//                reader.close();
+//            } catch (IOException e) {
+//                return -1;
+//            }
+//            catch (Exception e) {
+//                return -1;
+//            }
+//            return 0;
+//        }
+//    }
+//}
